87f6c5cc4616f99ade9c692cac5d480d6b029264
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-08 15:52:18 +0200

    added base

--- carapp/src/main/java/de/lindele/carapp/car/service/CarService.java
+++ carapp/src/main/java/de/lindele/carapp/car/service/CarService.java
@@ -1,4 +1,23 @@
 package de.lindele.carapp.car.service;
 
+
+import de.lindele.carapp.car.controller.model.CarWebModel;
+import de.lindele.carapp.car.repository.CarRepository;
+import de.lindele.carapp.car.service.model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CarService {
+
+    @Autowired
+    CarRepository carRepository;
+
+
+
+    Car createCar(Car car) {
+        carRepository.save(car);
+    }
+
+
 }
