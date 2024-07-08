49c291bec08cae9d6a653875bb8e784ad1f01a60
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-08 15:50:09 +0200

    added placeholder

--- carapp/src/main/java/de/lindele/carapp/rental/service/model/Rental.java
+++ carapp/src/main/java/de/lindele/carapp/rental/service/model/Rental.java
@@ -1,4 +1,21 @@
 package de.lindele.carapp.rental.service.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
 public class Rental {
+
+    private Long id;
+    private Long customerId;
+    private Long carId;
+    private Date rentalDate;
+    private Date returnDate;
+    private Integer kilometers;
+
+
 }
