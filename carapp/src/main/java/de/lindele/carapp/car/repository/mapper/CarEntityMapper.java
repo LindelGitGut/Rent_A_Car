cf8144d97839666412be3b19baea3250d1479a82
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-09 15:46:08 +0200

    created basic mapping methods

--- carapp/src/main/java/de/lindele/carapp/car/repository/mapper/CarEntityMapper.java
+++ carapp/src/main/java/de/lindele/carapp/car/repository/mapper/CarEntityMapper.java
@@ -1,10 +1,37 @@
 package de.lindele.carapp.car.repository.mapper;
 
 
+import de.lindele.carapp.car.repository.model.CarEntity;
+import de.lindele.carapp.car.service.model.Car;
 import org.springframework.stereotype.Component;
 
 
-//TODO Implement Mapping
+
 @Component
-public class CarEntityMapper {
+public  class CarEntityMapper {
+
+    public Car map(CarEntity carEntity) {
+        Car car = new Car();
+        car.setMake(carEntity.getBrand());
+        car.setModel(carEntity.getModel());
+        car.setRegistrationNumber(carEntity.getRegistrationNumber());
+        car.setDrivenkilometers(carEntity.getKilometer());
+        car.setPricePerKilometer(carEntity.getPricePerKilometer());
+        car.setColor(carEntity.getColor());
+        return car;
+    }
+
+    public CarEntity map(Car car) {
+        CarEntity carEntity = new CarEntity();
+        carEntity.setBrand(car.getMake());
+        carEntity.setModel(car.getModel());
+        carEntity.setRegistrationNumber(car.getRegistrationNumber());
+        carEntity.setKilometer(car.getDrivenkilometers());
+        carEntity.setPricePerKilometer(car.getPricePerKilometer());
+        carEntity.setColor(car.getColor());
+
+        return carEntity;
+    }
+
+
 }
