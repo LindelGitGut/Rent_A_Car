d00851cb39bd8c8707a5278fea38d47c2031aa1f
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-09 15:48:03 +0200

    added attributes

--- carapp/src/main/java/de/lindele/carapp/car/controller/model/CarWebModel.java
+++ carapp/src/main/java/de/lindele/carapp/car/controller/model/CarWebModel.java
@@ -2,5 +2,36 @@ package de.lindele.carapp.car.controller.model;
 
 
 //TODO define model
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class CarWebModel {
+
+    //TODO check which Properties are needed for Webmodel
+
+    private String make;
+    private String model;
+    private String registrationNumber;
+    private String color;
+    private Integer drivenkilometers;
+    private double pricePerKilometer;
+
+    @Override
+    public String toString() {
+        return "CarWebModel{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                ", color='" + color + '\'' +
+                ", drivenkilometers=" + drivenkilometers +
+                ", pricePerKilometer=" + pricePerKilometer +
+                '}';
+    }
 }
