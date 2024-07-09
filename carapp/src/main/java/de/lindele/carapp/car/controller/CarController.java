7cf7198823d5c9939c1cfe0d4f70e3a65aadca1e
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-09 15:45:34 +0200

    added base operation using service

--- carapp/src/main/java/de/lindele/carapp/car/controller/CarController.java
+++ carapp/src/main/java/de/lindele/carapp/car/controller/CarController.java
@@ -1,12 +1,17 @@
 package de.lindele.carapp.car.controller;
 
 
+import de.lindele.carapp.car.controller.mapper.CarWebModelMapper;
 import de.lindele.carapp.car.controller.model.CarWebModel;
 import de.lindele.carapp.car.service.CarService;
 import de.lindele.carapp.car.service.model.Car;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController("/car")
@@ -14,7 +19,10 @@ import org.springframework.web.bind.annotation.*;
 public class CarController {
 
     @Autowired
-    CarService carService;
+    private CarService carService;
+
+    @Autowired
+    private CarWebModelMapper carWebModelMapper;
 
 
 
@@ -23,34 +31,63 @@ public class CarController {
     //CREATE
 
     @PostMapping
-    <CarWebModel> ResponseEntity<CarWebModel> createCar(@RequestBody CarWebModel carWebModel) {
-        return ResponseEntity.ok(carService.createCar(carWebModel));
-    }
+    ResponseEntity<CarWebModel> createCar(@RequestBody CarWebModel carWebModel) {
+        //convert CarWebModel to Car
+        Car car = carWebModelMapper.map(carWebModel);
+        //create car and return CarWebModel
+        return ResponseEntity.ok(carWebModelMapper.map(carService.createCar(car)));
 
+    }
 
     //READ
-    @GetMapping
-    <CarWebModel> ResponseEntity<CarWebModel> getCar(@PathVariable Long id) {
-        return ResponseEntity.ok(carService.getCar(id));
+    @GetMapping("/{id}")
+    ResponseEntity<CarWebModel> getCar(@PathVariable Long id) {
+        return ResponseEntity.ok(carWebModelMapper.map(carService.getCarById(id)));
     }
+
+    @GetMapping
+    ResponseEntity<Page<CarWebModel>> getAllCars(
+            @RequestParam(required = false) String brand,
+            @RequestParam(required = false) String color,
+            @PageableDefault(size = 10) Pageable pageable
+    ) {
+
+        Page<Car> cars;
+        if (StringUtils.hasText(brand)) {
+            cars = carService.getAllCarsByBrand(brand, pageable);
+            return ResponseEntity.ok(cars.map(carWebModelMapper::map));
+        }
+
+        if (StringUtils.hasText(color)) {
+            cars = carService.getAllCarsByColor(color, pageable);
+            return ResponseEntity.ok(cars.map(carWebModelMapper::map));
+        }
+
+        if(StringUtils.hasText(brand) && StringUtils.hasText(color)){
+            cars = carService.getAllCarsByBrandAndColor(brand, color, pageable);
+            return ResponseEntity.ok(cars.map(carWebModelMapper::map));
+        }
+
+        cars = carService.getAllCars();
+        return ResponseEntity.ok(cars.map(carWebModelMapper::map));
+    }
+
     //UPDATE
     @PutMapping
-    <CarWebModel> ResponseEntity<CarWebModel> updateCar(@RequestBody CarWebModel carWebModel) {
-        return ResponseEntity.ok(carService.updateCar(carWebModel));
+     ResponseEntity<CarWebModel> updateCar(
+             @RequestBody CarWebModel carWebModel,
+            @RequestParam(required = true
+            ) Long id) {
+        //convert CarWebModel to Car
+        Car car = carWebModelMapper.map(carWebModel);
+        //update car and return CarWebModel
+        return ResponseEntity.ok(carWebModelMapper.map(carService.updateCar(car)));
     }
     //DELETE
     @DeleteMapping
-    <CarWebModel> ResponseEntity<CarWebModel> deleteCar(@PathVariable Long id) {
-        return ResponseEntity.ok(carService.deleteCar(id));
+     ResponseEntity<CarWebModel> deleteCar(@PathVariable Long id) {
+        carService.deleteCar(id);
+        return ResponseEntity.noContent().build();
     }
 
-    CarWebModel convertCarToCarWebModel(Car car) {
-        //TODO Implement
-        return null;
-    }
-
-    Car convertCarWebModelToCar(CarWebModel carWebModel) {
-        //TODO Implement
-        return null;
-    }
 }
