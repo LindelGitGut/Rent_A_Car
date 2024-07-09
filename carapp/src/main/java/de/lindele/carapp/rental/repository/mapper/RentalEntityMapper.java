2f2a4a28df6bf947db9fa86eb83827acbd1739b0
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-09 16:23:03 +0200

    commented out wron getters added TODO to fix later

--- carapp/src/main/java/de/lindele/carapp/rental/repository/mapper/RentalEntityMapper.java
+++ carapp/src/main/java/de/lindele/carapp/rental/repository/mapper/RentalEntityMapper.java
@@ -5,12 +5,14 @@ import de.lindele.carapp.customer.repository.model.CustomerEntity;
 import de.lindele.carapp.rental.repository.model.RentalEntity;
 import de.lindele.carapp.rental.service.model.Rental;
 
+//TODO fix Customer Mapping
+
 public class RentalEntityMapper {
 
     public static Rental entityToModel(RentalEntity entity) {
         Rental model = new Rental();
         model.setId(entity.getId());
-        model.setCustomerId(entity.getCustomer().getId());
+        //model.setCustomerId(entity.getCustomer().get());
         model.setCarId(entity.getCar().getId());
         model.setRentalDate(entity.getRentalDate());
         model.setReturnDate(entity.getReturnDate());
@@ -21,11 +23,11 @@ public class RentalEntityMapper {
     public static RentalEntity modelToEntity(Rental model, CustomerEntity customer, CarEntity car) {
         RentalEntity entity = new RentalEntity();
         entity.setId(model.getId());
-        entity.setCustomer(customer);
+       // entity.setCustomer(customer);
         entity.setCar(car);
         entity.setRentalDate(model.getRentalDate());
         entity.setReturnDate(model.getReturnDate());
-        entity.setKilometersDriven(model.getKilometersDriven());
+        //entity.setKilometersDriven(model.getKilometersDriven());
         return entity;
     }
 }
