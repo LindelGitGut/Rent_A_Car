0aea0647048c834311b942f694d274d6f795446f
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-09 16:22:20 +0200

    implemented methods from interface

--- carapp/src/main/java/de/lindele/carapp/car/repository/adapter/CarRepositoryAdapter.java
+++ carapp/src/main/java/de/lindele/carapp/car/repository/adapter/CarRepositoryAdapter.java
@@ -2,12 +2,16 @@ package de.lindele.carapp.car.repository.adapter;
 
 
 import de.lindele.carapp.car.repository.mapper.CarEntityMapper;
+import de.lindele.carapp.car.repository.model.CarEntity;
 import de.lindele.carapp.car.service.model.Car;
 import de.lindele.carapp.car.service.port.CarPersistencePort;
+import de.lindele.carapp.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 //TODO implement methods
@@ -25,18 +29,55 @@ public class CarRepositoryAdapter implements CarPersistencePort {
 
     @Override
     public Page<Car> findAllCars(Pageable pageable) {
-        return null;
+        return carRepository.findAll(pageable)
+                .map(carEntityMapper::map);
     }
 
     @Override
     public Car findCarById(Long id) {
-        return null;
+        return carRepository.findById(id)
+                .map(carEntityMapper::map)
+                .orElse(null);
     }
 
     @Override
-    public Car findAllCarsByColor(String color) {
-        return null;
+    public Page<Car> findAllCarsByColor(String color, Pageable pageable) {
+        return carRepository.findByColor(color, pageable)
+                .map(carEntityMapper::map);
     }
+
+    @Override
+    public Page<Car> findAllCarsByBrandAndColor(String brand, String color, Pageable pageable) {
+        return carRepository.findByBrandAndColor(brand, color, pageable)
+                .map(carEntityMapper::map);
+    }
+
+    @Override
+    public Car saveCar(Car car) {
+        CarEntity savedCar = carRepository.save(carEntityMapper.map(car));
+        return carEntityMapper.map(savedCar);
+    }
+
+    @Override
+    public Car updateCar(Car car, Long id) {
+        Optional<CarEntity> savedCar = carRepository.findById(id);
+        if (savedCar.isPresent()){
+            CarEntity updatedCar = carEntityMapper.map(car);
+            updatedCar.setId(id);
+            return carEntityMapper.map(carRepository.save(updatedCar));
+        }
+        else throw new ResourceNotFoundException("Car not found with id: " + id);
+    }
+
+    @Override
+    public void deleteCar(Long id) {
+        if (carRepository.existsById(id)) {
+            carRepository.deleteById(id);
+        }
+        else throw new ResourceNotFoundException("Car not found with id: " + id);
+    }
+
+
 
 
 }
