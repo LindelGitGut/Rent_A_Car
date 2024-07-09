a02f110f4b95b7f0891234df3e5c319c40c59fad
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-09 15:48:26 +0200

    added basic mapper methods

--- carapp/src/main/java/de/lindele/carapp/car/controller/mapper/CarWebModelMapper.java
+++ carapp/src/main/java/de/lindele/carapp/car/controller/mapper/CarWebModelMapper.java
@@ -1,10 +1,41 @@
 package de.lindele.carapp.car.controller.mapper;
 
 
+import de.lindele.carapp.car.controller.model.CarWebModel;
+import de.lindele.carapp.car.service.model.Car;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 
 //TODO Implement Mapping
 @Component
 public class CarWebModelMapper {
+
+
+    public Car map(CarWebModel carWebModel) {
+
+        //TODO Test is properties are copied correctly, testing
+
+        Car car = new Car();
+        car.setModel(car.getModel());
+        car.setMake(carWebModel.getMake());
+        car.setDrivenkilometers(carWebModel.getDrivenkilometers());
+        car.setRegistrationNumber(car.getRegistrationNumber());
+        car.setPricePerKilometer(car.getPricePerKilometer());
+        car.setColor(carWebModel.getColor());
+        return car;
+    }
+
+    public CarWebModel map(Car car) {
+        CarWebModel carWebModel= new CarWebModel();
+        carWebModel.setModel(car.getModel());
+        carWebModel.setMake(car.getMake());
+        carWebModel.setDrivenkilometers(car.getDrivenkilometers());
+        carWebModel.setRegistrationNumber(car.getRegistrationNumber());
+        carWebModel.setPricePerKilometer(car.getPricePerKilometer());
+        carWebModel.setColor(car.getColor());
+        return carWebModel;
+    }
+
+
 }
