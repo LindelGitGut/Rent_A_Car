439f8cfa98e1259cfa8bef5cea0daffd3ff656ab
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-10 11:05:50 +0200

    -added attributes

--- carapp/src/main/java/de/lindele/carapp/customer/controller/model/CustomerWebModel.java
+++ carapp/src/main/java/de/lindele/carapp/customer/controller/model/CustomerWebModel.java
@@ -1,4 +1,25 @@
 package de.lindele.carapp.customer.controller.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class CustomerWebModel {
+
+    private String firstname;
+    private String lastname;
+    private String address;
+    private Date birthdate;
+    private String city;
+    private String postalCode;
+    private String phoneNumber;
+    private String email;
+
+
 }
