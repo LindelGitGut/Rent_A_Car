972f5f5a286bc9521cec1f74847c928abf4a3fdf
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-10 11:41:31 +0200

    -added needed methods

--- carapp/src/main/java/de/lindele/carapp/customer/repository/adapter/CustomerRepository.java
+++ carapp/src/main/java/de/lindele/carapp/customer/repository/adapter/CustomerRepository.java
@@ -1,7 +1,18 @@
 package de.lindele.carapp.customer.repository.adapter;
 
+import de.lindele.carapp.customer.repository.mapper.CustomerEntityMapper;
 import de.lindele.carapp.customer.repository.model.CustomerEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long>{
+
+    Page<CustomerEntity> findByFirstName(String firstName, Pageable pageable);
+
+    Page<CustomerEntity> findByLastName(String lastName, Pageable pageable);
+
+    Page<CustomerEntity> findByFirstNameAndLastName(String firstName, String lastName, Pageable pageable);
 }
