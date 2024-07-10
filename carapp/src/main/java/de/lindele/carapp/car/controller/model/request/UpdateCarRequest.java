26d22f520a5cbe5ee79c50e14d14b8e7e2c5fcc4
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-10 14:06:20 +0200

    created Update Car request

---
+++ carapp/src/main/java/de/lindele/carapp/car/controller/model/request/UpdateCarRequest.java
@@ -0,0 +1,20 @@
+package de.lindele.carapp.car.controller.model.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UpdateCarRequest {
+    private Long id;
+    private String brand;
+    private String model;
+    private String color;
+    private String registrationNumber;
+    private int kilometer; // Kilometerstand
+    private double pricePerKilometer;
+
+}
