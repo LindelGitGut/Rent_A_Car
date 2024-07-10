61b4fbd3cab8aa0794651a6168b06f6ea972c007
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-10 11:04:26 +0200

    added map methods

--- carapp/src/main/java/de/lindele/carapp/customer/repository/mapper/CustomerEntityMapper.java
+++ carapp/src/main/java/de/lindele/carapp/customer/repository/mapper/CustomerEntityMapper.java
@@ -1,9 +1,46 @@
 package de.lindele.carapp.customer.repository.mapper;
 
 
+import de.lindele.carapp.customer.repository.model.CustomerEntity;
+import de.lindele.carapp.customer.service.model.Customer;
 import org.springframework.stereotype.Component;
 
-//TODO Implement Mapping
+//TODO check if works as suggested
 @Component
 public class CustomerEntityMapper {
+
+
+
+    public CustomerEntity map (Customer customer){
+        CustomerEntity customerEntity = new CustomerEntity();
+      customerEntity.setAddress(customer.getAddress());
+      customerEntity.setBirthdate(customer.getBirthdate());
+      customerEntity.setId(customer.getId());
+      customerEntity.setEmail(customer.getEmail());
+      customerEntity.setPhoneNumber(customer.getPhoneNumber());
+      customerEntity.setFirstname(customer.getFirstname());
+        customerEntity.setLastname(customer.getLastname());
+        customerEntity.setPostalCode(customer.getPostalCode());
+        customerEntity.setCity(customer.getCity());
+
+
+        return customerEntity;
+    }
+
+
+    public Customer map (CustomerEntity customerEntity){
+        Customer customer = new Customer();
+        customer.setId(customerEntity.getId());
+        customer.setAddress(customerEntity.getAddress());
+        customer.setBirthdate(customerEntity.getBirthdate());
+        customer.setEmail(customerEntity.getEmail());
+        customer.setPhoneNumber(customerEntity.getPhoneNumber());
+        customer.setFirstname(customerEntity.getFirstname());
+        customer.setLastname(customerEntity.getLastname());
+
+        return customer;
+    }
+
+
+
 }
