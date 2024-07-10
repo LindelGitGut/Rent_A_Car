ef26b86d3852628525959bf0266d125202402245
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-10 11:04:10 +0200

    added attributes

--- carapp/src/main/java/de/lindele/carapp/customer/service/model/Customer.java
+++ carapp/src/main/java/de/lindele/carapp/customer/service/model/Customer.java
@@ -1,4 +1,23 @@
 package de.lindele.carapp.customer.service.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Customer {
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String address;
+    private Date birthdate;
+    private String city;
+    private String postalCode;
+    private String phoneNumber;
+    private String email;
 }
