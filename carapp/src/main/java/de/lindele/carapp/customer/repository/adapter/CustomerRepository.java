7aeaa297e7758a22a0b89f79dae682c91a2e6378
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-10 14:05:16 +0200

    added repository annotation

--- carapp/src/main/java/de/lindele/carapp/customer/repository/adapter/CustomerRepository.java
+++ carapp/src/main/java/de/lindele/carapp/customer/repository/adapter/CustomerRepository.java
@@ -5,9 +5,12 @@ import de.lindele.carapp.customer.repository.model.CustomerEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
+@Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long>{
 
     Page<CustomerEntity> findByFirstName(String firstName, Pageable pageable);
