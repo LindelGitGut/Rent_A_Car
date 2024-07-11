00f5741052f5e2ea02a6fd7415a64bcff4e83082
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-11 09:35:07 +0200

    gave some attributes

--- carapp/src/main/java/de/lindele/carapp/rental/controller/model/RentalWebModel.java
+++ carapp/src/main/java/de/lindele/carapp/rental/controller/model/RentalWebModel.java
@@ -1,4 +1,41 @@
 package de.lindele.carapp.rental.controller.model;
 
+import de.lindele.carapp.car.repository.model.CarEntity;
+import de.lindele.carapp.car.service.model.Car;
+import de.lindele.carapp.customer.repository.model.CustomerEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class RentalWebModel {
+
+    @NotNull
+    private Long rentalNumber;
+
+//    @NotNull
+//    @NotBlank
+//    private String customerName;
+
+    @NotNull
+    private Car car;
+
+    @NotNull
+    private Date rentalDate;
+
+    @NotNull
+    private Date returnDate;
+
+    @NotNull
+    private int rentalKilometers;
 }
