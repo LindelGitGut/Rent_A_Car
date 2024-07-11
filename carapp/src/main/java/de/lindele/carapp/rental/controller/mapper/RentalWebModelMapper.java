625c67468b5d90f05213c2a6d6d7b952d3782769
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-11 09:35:37 +0200

    added mapper method

--- carapp/src/main/java/de/lindele/carapp/rental/controller/mapper/RentalWebModelMapper.java
+++ carapp/src/main/java/de/lindele/carapp/rental/controller/mapper/RentalWebModelMapper.java
@@ -1,4 +1,23 @@
 package de.lindele.carapp.rental.controller.mapper;
 
+import de.lindele.carapp.rental.controller.model.RentalWebModel;
+import de.lindele.carapp.rental.service.model.Rental;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class RentalWebModelMapper {
+
+    RentalWebModel map (Rental rental){
+        RentalWebModel rentalWebModel= new RentalWebModel();
+        rentalWebModel.setRentalNumber(rental.getId());
+        rentalWebModel.setCar(rental.getCar());
+        rentalWebModel.setRentalDate(rental.getRentalDate());
+        rentalWebModel.setReturnDate(rental.getReturnDate());
+        rentalWebModel.setRentalKilometers(rental.getKilometers());
+        return rentalWebModel;
+
+    }
+
+
 }
