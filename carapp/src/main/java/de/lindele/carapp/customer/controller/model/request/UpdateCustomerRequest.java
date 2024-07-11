d822321c3dab55f8b0a22d3b41e36195241cd5e2
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-11 09:35:52 +0200

    created request

--- carapp/src/main/java/de/lindele/carapp/customer/controller/model/request/UpdateCustomerRequest.java
+++ carapp/src/main/java/de/lindele/carapp/customer/controller/model/request/UpdateCustomerRequest.java
@@ -1,4 +1,40 @@
 package de.lindele.carapp.customer.controller.model.request;
 
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class UpdateCustomerRequest {
+    @NotNull
+    @NotBlank
+    private String firstname;
+    @NotNull
+    @NotBlank
+    private String lastname;
+    @NotNull
+    @NotBlank
+    private String address;
+    @NotNull
+    private Date birthdate;
+    @NotNull
+    @NotBlank
+    private String city;
+    @NotNull
+    @NotBlank
+    private String postalCode;
+    @NotNull
+    @NotBlank
+    private String phoneNumber;
+    @NotNull
+    @NotBlank
+    private String email;
+
 }
