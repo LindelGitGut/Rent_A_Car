ef7a3ca3209df5d6b396a4f6c1520ebab75717f2
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-12 11:25:17 +0200

    switched to builder

--- carapp/src/main/java/de/lindele/carapp/customer/controller/mapper/UpdateCustomerRequestMapper.java
+++ carapp/src/main/java/de/lindele/carapp/customer/controller/mapper/UpdateCustomerRequestMapper.java
@@ -9,15 +9,26 @@ import org.springframework.stereotype.Component;
 public class UpdateCustomerRequestMapper {
 
   public Customer map(UpdateCustomerRequest updateCustomerRequest) {
-    Customer customer = new Customer();
-    customer.setFirstname(updateCustomerRequest.getFirstname());
-    customer.setLastname(updateCustomerRequest.getLastname());
-    customer.setAddress(updateCustomerRequest.getAddress());
-    customer.setBirthdate(updateCustomerRequest.getBirthdate());
-    customer.setCity(updateCustomerRequest.getCity());
-    customer.setPostalCode(updateCustomerRequest.getPostalCode());
-    customer.setPhoneNumber(updateCustomerRequest.getPhoneNumber());
-    customer.setEmail(updateCustomerRequest.getEmail());
-    return customer;
+    return Customer.builder()
+        .firstname(updateCustomerRequest.getFirstname())
+        .lastname(updateCustomerRequest.getLastname())
+        .address(updateCustomerRequest.getAddress())
+        .birthdate(updateCustomerRequest.getBirthdate())
+        .city(updateCustomerRequest.getCity())
+        .postalCode(updateCustomerRequest.getPostalCode())
+        .phoneNumber(updateCustomerRequest.getPhoneNumber())
+        .email(updateCustomerRequest.getEmail())
+        .build();
+
+    //            new Customer();
+    //    customer.setFirstname(updateCustomerRequest.getFirstname());
+    //    customer.setLastname(updateCustomerRequest.getLastname());
+    //    customer.setAddress(updateCustomerRequest.getAddress());
+    //    customer.setBirthdate(updateCustomerRequest.getBirthdate());
+    //    customer.setCity(updateCustomerRequest.getCity());
+    //    customer.setPostalCode(updateCustomerRequest.getPostalCode());
+    //    customer.setPhoneNumber(updateCustomerRequest.getPhoneNumber());
+    //    customer.setEmail(updateCustomerRequest.getEmail());
+    // return customer;
   }
 }
