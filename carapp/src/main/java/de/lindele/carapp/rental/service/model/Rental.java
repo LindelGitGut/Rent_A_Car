a4369995cfc0d2efedef9c87d31587ce97d2c1e8
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-12 09:21:48 +0200

    - annotated with Builder and Data

--- carapp/src/main/java/de/lindele/carapp/rental/service/model/Rental.java
+++ carapp/src/main/java/de/lindele/carapp/rental/service/model/Rental.java
@@ -2,15 +2,16 @@ package de.lindele.carapp.rental.service.model;
 
 import de.lindele.carapp.car.service.model.Car;
 import java.util.Date;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
+import de.lindele.carapp.customer.service.model.Customer;
+import lombok.*;
+
+@Data
+@Builder
+@RequiredArgsConstructor
 public class Rental {
-
   private Long id;
-  private Long customerId;
+  private Customer customer;
   private Car car;
   private Date rentalDate;
   private Date returnDate;
