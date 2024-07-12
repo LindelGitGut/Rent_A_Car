34cc17aef62a4b767fa97b5a705963fba6917f66
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-12 11:25:29 +0200

    added Test Class

---
+++ carapp/src/test/java/de/lindele/carapp/customer/controller/CustomerControllerIntegrationTest.java
@@ -0,0 +1,23 @@
+package de.lindele.carapp.customer.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerControllerIntegrationTest {
+
+  @Test
+  void createCustomer() {}
+
+  @Test
+  void getCustomer() {}
+
+  @Test
+  void getAllCustomers() {}
+
+  @Test
+  void updateCustomer() {}
+
+  @Test
+  void deleteCustomer() {}
+}
